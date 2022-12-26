dcd61e83b11b0e78ed74348357ac7bac93bd52ef
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2022-12-26 11:38:56 +0530

    Favicon and title changed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>SAE-SLIET</p>
-      </header>
-    </div>
+    <>
+    
+    </>
   );
 }
 
