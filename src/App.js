05eaf60f39ebc5b799a7ee0e68b663de9311a0c1
Rohan Kumar Chaudhary <rohankumarchaudhary67@gmail.com>
2022-12-28 15:46:30 +0530

    Final Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './App.css';
+
+import {Navbar, Header} from './components/index'
 
 const App = () => (
     <div className="app">
         <div className="gradient__bg">
-            
+            <Navbar />
+            <Header />
         </div>
     </div>
 )
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,43 @@
-import React from 'react'
-import './NavBar.css'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/sae-sliet-logos.jpg';
+import './NavBar.css';
 
-export default function NavBar() {
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
-        <div>NavBar</div>
-    )
-}
+        <div className="sae__navbar">
+        <div className="sae__navbar-links">
+            <div className="sae__navbar-links_logo">
+            <img src={logo} alt="No" />
+            </div>
+            <div className="sae__navbar-links_container">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wsae">Gallery</a></p>
+            <p><a href="#possibility">Teams</a></p>
+            <p><a href="#features">Blog</a></p>
+            <p><a href="#blog">Contact</a></p>
+            </div>
+        </div>
+        <div className="sae__navbar-menu">
+            {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+            {toggleMenu && (
+            <div className="sae__navbar-menu_container scale-up-center">
+            <div className="sae__navbar-menu_container-links">
+                <p><a href="#home">Home</a></p>
+                <p><a href="#wsae">Gallery</a></p>
+                <p><a href="#possibility">Teams</a></p>
+                <p><a href="#features">Blog</a></p>
+                <p><a href="#blog">Contact</a></p>
+            </div>
+            </div>
+            )}
+        </div>
+        </div>
+    );
+};
+
+export default Navbar;
